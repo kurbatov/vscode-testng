7c55d13b6fc42ee55168bcbad04f219f09aeef4d
Oleg Kurbatov <o.v.kurbatov@gmail.com>
2022-07-16 23:39:54 +0300

    Add dependency between test groups

--- submodule/src/test/java/com/github/kurbatov/TestNGTest.java
+++ submodule/src/test/java/com/github/kurbatov/TestNGTest.java
@@ -6,8 +6,13 @@ import static org.testng.Assert.assertTrue;
 
 public class TestNGTest  {
 
-    @Test
-    public void shouldAnswerWithTrue() {
+    @Test(groups = {"foo"})
+    public void firstTest() {
+        System.out.println("First test");
+    }
+
+    @Test(dependsOnGroups = {"foo"})
+    public void dependentTest() {
         System.out.println("Testing");
         assertTrue(true);
     }
