021185fa3d7ffa49c19bc6a399d708bf75cc71b3
Oleg Kurbatov <o.v.kurbatov@gmail.com>
2022-07-16 23:23:46 +0300

    First commit

---
+++ submodule/src/test/java/com/github/kurbatov/TestNGTest.java
@@ -0,0 +1,14 @@
+package com.github.kurbatov;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertTrue;
+
+public class TestNGTest  {
+
+    @Test
+    public void shouldAnswerWithTrue() {
+        System.out.println("Testing");
+        assertTrue(true);
+    }
+}
